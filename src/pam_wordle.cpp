9af6ed013a7d38192e1391c1c40de8b4bc23b143
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-28 08:54:44 +0000

    use C++ string

--- src/pam_wordle.cpp
+++ src/pam_wordle.cpp
@@ -1,33 +1,32 @@
 #include "pam_wordle.h"
 #include <stdio.h>
 #include <cstring>
+#include <string>
+#include <iostream>
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *handle, int flags, int argc, const char **argv)
 {
     int pam_code;
-    const char *answer = "hello";
-    const char *guess = nullptr;
+    std::string answer = "hello";
+    std::string guess;
     int num_guesses = 6;
 
     for (int i = 0; i < num_guesses; ++i) {
         /* Asking the user for a guess */
-        pam_code = pam_get_user(handle, &guess, "guess: ");
-        if (pam_code != PAM_SUCCESS) {
-            fprintf(stderr, "Please make a guess.");
+        fprintf(stdout, "Your guess[%d/%d]: ", i + 1, num_guesses);
+        std::cin >> guess;
+
+        if (guess.length() != 5) {
+            fprintf(stderr, "Please input a word with 5 letters.\r\n");
             return PAM_PERM_DENIED;
         }
 
-        if (guess == nullptr || strlen(guess) != 5) {
-            fprintf(stderr, "Please input a word with 5 letters.");
-            return PAM_PERM_DENIED;
-        }
-
-        if (strcmp(guess, answer) == 0) {
+        if (guess == answer) {
             return PAM_SUCCESS;
         }
     }
 
-    fprintf(stderr, "Maximum number of guess reached. It was %s\r\n", answer);
+    fprintf(stderr, "Maximum number of guess reached. It was %s\r\n", answer.c_str());
     return PAM_PERM_DENIED;
 }
 
