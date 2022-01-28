bfec3b334650a63222e15e42e01c60f859d02c01
Cocoa <r.xu.1@research.gla.ac.uk>
2022-01-28 09:06:54 +0000

    add coloured output

--- src/pam_wordle.cpp
+++ src/pam_wordle.cpp
@@ -3,12 +3,20 @@
 #include <cstring>
 #include <string>
 #include <iostream>
+#include <map>
+#include <vector>
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *handle, int flags, int argc, const char **argv)
 {
     int pam_code;
     std::string answer = "hello";
+    std::map<char, bool> exists;
+    for (auto &c : answer) {
+        exists[c] = true;
+    }
+
     std::string guess;
+    std::vector<std::string> guesses;
     int num_guesses = 6;
 
     for (int i = 0; i < num_guesses; ++i) {
@@ -19,6 +27,22 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *handle, int flags, int argc, co
         if (guess.length() != 5) {
             fprintf(stderr, "Please input a word with 5 letters.\r\n");
             return PAM_PERM_DENIED;
+        }
+
+        std::string result;
+        for (int j = 0; j < 5; ++j) {
+            if (answer[j] == guess[j]) {
+                result += "\033[92;102m██\033[m ";
+            } else if (exists[guess[j]]) {
+                result += "\033[93;103m██\033[m ";
+            } else {
+                result += "\033[90;100m██\033[m ";
+            }
+        }
+        guesses.push_back(result);
+
+        for (auto & guessed : guesses) {
+            fprintf(stdout, "%s\r\n", guessed.c_str());
         }
 
         if (guess == answer) {
